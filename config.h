2adbe74f802178bc3ef188eefdba603b4825b6e0
Dominic <dominic.lehner@arcor.de>
2019-10-27 08:15:25 +0100

    limod keymap

---
+++ config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// place overrides here
+#define NO_ACTION_ONESHOT
+
+//#undef TAPPING_TERM
+//#define TAPPING_TERM 400
+
+#define IGNORE_MOD_TAP_INTERRUPT
---
+++ keymap.c
@@ -0,0 +1,129 @@
+/*
+Copyright 2017 Balz Guenat <balz.guenat@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "keymap_german.h"
+#include "keymap_nordic.h"
+
+#define BASE 0
+#define SYMBS 2
+#define MOUSE 4
+#define NAV 3
+#define FKEYS 1
+
+
+enum {
+  TD_COMMA = 1,
+  TD_ARROW_LEFT = 2,
+  TD_ARROW_RIGHT = 3,
+  TD_POS = 4,
+  TD_END = 5,
+  TD_PGUP = 6,
+  TD_PGDN = 7,
+  TD_ARROW_UP = 8,
+  TD_ARROW_DOWN = 9,
+  TD_SHIFT = 10,
+};
+
+//Tap Dance Definitions  https://github.com/qmk/qmk_firmware/issues/859
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_COMMA]  = ACTION_TAP_DANCE_DOUBLE(KC_COMMA,DE_SCLN),
+  [TD_PGUP]  = ACTION_TAP_DANCE_DOUBLE(KC_X,KC_PGUP),
+  [TD_PGDN]  = ACTION_TAP_DANCE_DOUBLE(KC_C,KC_PGDN),
+  [TD_ARROW_LEFT]  = ACTION_TAP_DANCE_DOUBLE(KC_N,KC_LEFT),
+  [TD_ARROW_RIGHT]  = ACTION_TAP_DANCE_DOUBLE(KC_M,KC_RIGHT),
+  [TD_ARROW_UP]  = ACTION_TAP_DANCE_DOUBLE(KC_COMMA,KC_UP),
+  [TD_ARROW_DOWN]  = ACTION_TAP_DANCE_DOUBLE(KC_DOT,KC_DOWN),
+  [TD_POS]  = ACTION_TAP_DANCE_DOUBLE(KC_N,KC_HOME),
+  [TD_END]  = ACTION_TAP_DANCE_DOUBLE(KC_SLSH,KC_END),
+  [TD_SHIFT]  = ACTION_TAP_DANCE_DOUBLE(KC_LSHIFT, KC_CAPSLOCK)
+};
+
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
+    /* 0: plain Qwerty without layer switching
+     *         ,---------------. ,---------------. ,---------------.
+     *         |F13|F14|F15|F16| |F17|F18|F19|F20| |F21|F22|F23|F24|
+     * ,---.   |---------------| |---------------| |---------------| ,-----------. ,---------------. ,-------.
+     * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau| |VDn|VUp|Mut|Pwr| | Help  |
+     * `---'   `---------------' `---------------' `---------------' `-----------' `---------------' `-------'
+     * ,-----------------------------------------------------------. ,-----------. ,---------------. ,-------.
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|JPY|Bsp| |Ins|Hom|PgU| |NmL|  /|  *|  -| |Stp|Agn|
+     * |-----------------------------------------------------------| |-----------| |---------------| |-------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  | |Del|End|PgD| |  7|  8|  9|  +| |Mnu|Und|
+     * |-----------------------------------------------------------| `-----------' |---------------| |-------|
+     * |CapsL |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  :|  #|Retn|               |  4|  5|  6|KP,| |Sel|Cpy|
+     * |-----------------------------------------------------------|     ,---.     |---------------| |-------|
+     * |Shft|  <|  Z|  X|  C|  V|  B|  N|  M|  ,|  ,|  /| RO|Shift |     |Up |     |  1|  2|  3|KP=| |Exe|Pst|
+     * |-----------------------------------------------------------| ,-----------. |---------------| |-------|
+     * |Ctl|Gui|Alt|MHEN|HNJ| Space  |H/E|HENK|KANA|Alt|Gui|App|Ctl| |Lef|Dow|Rig| |  0    |  .|Ent| |Fnd|Cut|
+     * `-----------------------------------------------------------' `-----------' `---------------' `-------'
+     */
+    [BASE] = LAYOUT_all(
+                      KC_F13,  KC_F14,  KC_F15,  KC_F16, KC_F17, KC_F18, KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,
+    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,               KC_PSCR, KC_SLCK, KC_PAUS,    KC_4, KC_4, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,     KC_HELP,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_JYEN, KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,    KC_STOP, KC_AGIN,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,   LT(MOUSE,KC_I), LT(SYMBS,KC_O),    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,     KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_MENU, KC_UNDO,
+    TT(NAV), KC_A,    MT(MOD_LALT,KC_S), MT(MOD_LSFT,KC_D),  MT(MOD_LCTL,KC_F),    MT(MOD_LGUI,KC_G),   MT(MOD_LGUI,KC_H),   MT(MOD_LCTL,KC_J),  MT(MOD_LSFT, KC_K),	MT(MOD_LALT,KC_L),    KC_SCLN, KC_QUOT,          KC_NUHS, KC_ENT,                                    KC_P4,   KC_P5,   KC_P6,   KC_PCMM,    KC_SLCT, KC_COPY,
+    TD(TD_SHIFT), KC_NUBS, KC_Z,     TD(TD_PGUP),   TD(TD_PGDN),    KC_V,   TD(TD_POS),    TD(TD_ARROW_LEFT),    TD(TD_ARROW_RIGHT), TD(TD_ARROW_UP), TD(TD_ARROW_DOWN),  TD(TD_END),          KC_RO,   KC_RSFT,              KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PEQL,    KC_EXEC, KC_PSTE,
+    KC_LALT, KC_LGUI, KC_LCTL, KC_MHEN, KC_HANJ,         KC_SPC,         KC_HAEN, KC_HENK, KC_KANA, KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT, KC_PENT,    KC_FIND, KC_CUT
+    ),
+    [MOUSE] = LAYOUT_all(
+                      ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,
+    ______,           ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,             ______,______,______,    ______,______,______,______,    ______,
+    ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,______,______,______,    ______,______,
+    ______,  ______,  KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, KC_MS_WH_UP, ______,  ______,  ______, ______,  ______, ______, ______,         ______,     ______,______,______,    ______,______,______,______,    ______,______,
+    ______,  ______,  KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, ______,  ______,  ______, ______,  ______, ______,         ______, ______,                              ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______,         ______, ______,            ______,           ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______,         ______,           ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,       ______,______,    ______,______
+    ),
+    [NAV] = LAYOUT_all(
+                      ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,
+    ______,           ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,             ______,______,______,    ______,______,______,______,    ______,
+    ______,  ______,  ______,  ______,  ______, ______, ______,  ______, ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______, ______, ______,  KC_HOME,  KC_UP, KC_END,  ______, ______, ______,         ______,     ______,______,______,    ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______, ______, ______,  KC_LEFT,  KC_DOWN, KC_RIGHT,  ______, ______,         ______, ______,                              ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______, ______, TD(TD_POS),    TD(TD_ARROW_LEFT),    TD(TD_ARROW_RIGHT), TD(TD_ARROW_UP), TD(TD_ARROW_DOWN),  TD(TD_END),         ______, ______,            ______,           ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______,         ______,           ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,       ______,______,    ______,______
+    ),
+    [SYMBS] = LAYOUT_all(
+                      ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,
+    ______,           ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,             ______,______,______,    ______,______,______,______,    ______,
+    ______,  ______,  ______,  ______,  ______, ______, ______,  ______, ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  KC_MEDIA_PLAY_PAUSE,  KC_AUDIO_VOL_UP, ______, ______,  ______,  ______, ______,  ______, ______, ______,         ______,     ______,______,______,    ______,______,______,______,    ______,______,
+    ______,  ______,  KC_MEDIA_PREV_TRACK,  KC_MEDIA_NEXT_TRACK,  KC_AUDIO_VOL_DOWN, ______, ______,  ______,  ______, ______,  ______, ______,         ______, ______,                              ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______,         ______, ______,            ______,           ______,______,______,______,    ______,______,
+    ______,  ______,  ______,  ______,  ______,         ______,           ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,       ______,______,    ______,______
+    ),
+};
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
